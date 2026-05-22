a2934cfc29248a0ee528e688c29e942ce4af6462
aidanaa112233 <kurmanbaevaakmaral493@gmail.com>
2026-05-22 20:12:58 +0500

    .

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Experiment experiment = new Experiment();
+        Experiment experiment =
+                new Experiment();
 
         experiment.runMultipleTests();
 
